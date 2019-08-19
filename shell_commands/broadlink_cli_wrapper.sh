c2ed2b8694113fdf2adeffafae6c5791dbd15912
Rob Connolly <rob@webworxshop.com>
2019-08-19 20:51:15 +1200

    Fix another shellcheck error.

--- shell_commands/broadlink_cli_wrapper.sh
+++ shell_commands/broadlink_cli_wrapper.sh
@@ -2,4 +2,4 @@
 
 source /config/shell_commands/secrets.sh
 
-/config/shell_commands/broadlink_cli --type 0x2737 --host "$BROADLINK_IP" --mac "$BROADLINK_MAC" --send $1
+/config/shell_commands/broadlink_cli --type 0x2737 --host "$BROADLINK_IP" --mac "$BROADLINK_MAC" --send "$1"
