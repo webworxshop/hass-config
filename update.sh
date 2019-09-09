22c57735c53bb068d8ad95943478aabecaa4ffba
Rob Connolly <rob@webworxshop.com>
2019-09-10 08:52:57 +1200

    Fix shellcheck issue.

--- update.sh
+++ update.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+set -e
 
-cd /mnt/docker-data/home-assistant
+cd /mnt/docker-data/home-assistant || exit
 sudo apt update && sudo apt upgrade -y && sudo apt autoremove -y && sudo apt clean
 docker-compose pull
 docker-compose down
