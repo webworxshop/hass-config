ee9344d5c9e39c7947faff7c9929d44ad6baf612
Rob Connolly <rob@webworxshop.com>
2019-09-11 20:44:27 +1200

    Testing hass deployment.

--- update.sh
+++ update.sh
@@ -2,7 +2,6 @@
 set -e
 
 cd /mnt/docker-data/home-assistant || exit
-sudo apt update && sudo apt upgrade -y && sudo apt autoremove -y && sudo apt clean
 docker-compose pull
 docker-compose down
 docker-compose up -d
