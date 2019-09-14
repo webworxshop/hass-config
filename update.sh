ad0dd5b1472375fb908c2ce6ff3f32a177a2881b
Rob Connolly <rob@webworxshop.com>
2019-09-15 09:04:09 +1200

    Remove orphaned containers.

--- update.sh
+++ update.sh
@@ -4,7 +4,7 @@ set -e
 cd /mnt/docker-data/home-assistant || exit
 docker-compose -p ha pull
 docker-compose -p ha down
-docker-compose -p ha up -d
+docker-compose -p ha up -d --remove-orphans
 docker system prune -fa
 docker volume prune -f
 exit
